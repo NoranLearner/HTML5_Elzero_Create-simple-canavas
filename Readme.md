a4b8e015349d4001d3d95a8abde4672232b63ae6
noranmostafa843@gmail.com <55337446+NoranLearner@users.noreply.github.com>
2021-01-23 00:10:09 +0200

    HTML5_Elzero_Create-simple-canavas

---
+++ Readme.md
@@ -0,0 +1,7 @@
+
+Name: HTML5_Elzero_Create simple canavas
+
+Computer_Link: E:\5- GitHub\1- Front End\3- HTML5\1- Learn HTML5 In Arabic\28 - Canvas (Create Simple Canvas)
+
+Hosting_link:
+
---
+++ file.js
@@ -0,0 +1,25 @@
+var myCanvas = document.getElementById('c'),
+
+    myContext = myCanvas.getContext('2d');
+
+myContext.fillStyle = '#080';
+
+// myContext.fillRect(10, 10, 100, 50);
+
+// myContext.fillStyle = '#f00';
+
+// myContext.fillRect(20, 20, 100, 50);
+
+// myContext.fillStyle = '#00f';
+
+// myContext.fillRect(30, 30, 100, 50);
+
+// myContext.strokeStyle = '#ff0';
+
+// myContext.lineWidth = 10;
+
+// myContext.strokeRect(100, 100, 100, 50);
+
+myContext.fillRect(0, 0, 800, 400);
+
+myContext.clearRect(10, 10, 100, 100);
